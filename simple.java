fb8a2005220a5f6864072addd9c09a0edcf7ef27
Arunsaiprakash <aspanbuchelvan@gmail.com>
2016-05-03 21:30:47 -0700

    bug 1 code changes

--- simple.java
+++ simple.java
@@ -1,6 +1,11 @@
 public static sample{
 	public static void main(string[] args){
 		system.out.println("Hello");
+		display();
+	}
+
+public static void display(string[] args){
+		system.out.println("Hello");
 
 	}
 
